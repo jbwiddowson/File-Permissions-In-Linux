4368db5247275e70c12f145904a3f6cc24db1e84
Jacob <158038235+jbwiddowson@users.noreply.github.com>
2025-01-06 22:11:56 -0500

    Commands.sh

---
+++ Commands.sh
@@ -0,0 +1,11 @@
+| **Command**   | **Description**                                |
+|---------------|------------------------------------------------|
+| `ls`          | List files and directories.                   |
+| `ls -a`       | View hidden files                             |
+| `ls -l`       | View file permissions                         |
+| `ls -la       | Combines the two commands above               |
+| `cd`          | Change the current directory.                 |
+| `pwd`         | Print the current working directory.          |
+| `mkdir`       | Create a new directory.                       |
+| `rm`          | Delete files or directories (`-r` for recursive). |
+| `chmod`       | assists with changing permissions             |
